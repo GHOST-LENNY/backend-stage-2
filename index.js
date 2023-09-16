3587ee25c0abc790cbd8bcfac6b0873bcadf8f1d
lenny <lennyddon@gmail.com>
2023-09-16 13:24:43 +0100

    omitted code replaced for render

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
@@ -8,31 +8,30 @@ const app = express();
 // Middleware
 app.use(bodyParser.json());
 
-
 // Use environment variables for MongoDB connection
 const username = process.env.MONGODB_USERNAME;
 const password = process.env.MONGODB_PASSWORD;
 
-
 // Construct the MongoDB connection URI using the environment variables
-const uri = `mongodb+srv://${username}:${password}@stage2.lo2tefe.mongodb.net/person`
+const uri = `mongodb+srv://${username}:${password}@stage2.lo2tefe.mongodb.net/person`;
 
 // MongoDB Connection
-main().catch(err => console.log(err));
+main().catch((err) => console.log(err));
 
 async function main() {
   await mongoose.connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
-
 }
 
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => {
-  console.log('Connected to MongoDB');
-});
 
+//validation for render
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.once("open", () => {
+  console.log("Connected to MongoDB");
+});
 
 //Define Person Schema
 const personSchema = new mongoose.Schema({
@@ -46,7 +45,6 @@ const Person = mongoose.model("Person", personSchema);
 const David = new Person({ name: "David", track: "Design" });
 console.log(David);
 // David.save();
-
 
 // CRUD Endpoints
 app.post("/api", async (req, res) => {
@@ -63,45 +61,45 @@ app.post("/api", async (req, res) => {
   }
 });
 
-app.get('/api/:user_id', async (req, res) => {
-    try {
-      const person = await Person.findById(req.params.user_id);
-      if (!person) {
-        return res.status(404).json({ error: 'Person not found' });
-      }
-      res.json(person);
-    } catch (error) {
-      res.status(500).json({ error: 'Could not fetch person' });
+app.get("/api/:user_id", async (req, res) => {
+  try {
+    const person = await Person.findById(req.params.user_id);
+    if (!person) {
+      return res.status(404).json({ error: "Person not found" });
     }
-  });
-  
-  app.put('/api/:user_id', async (req, res) => {
-    try {
-      const updatedPerson = await Person.findByIdAndUpdate(
-        req.params.user_id,
-        req.body,
-        { new: true }
-      );
-      if (!updatedPerson) {
-        return res.status(404).json({ error: 'Person not found' });
-      }
-      res.json(updatedPerson);
-    } catch (error) {
-      res.status(500).json({ error: 'Could not update person' });
+    res.json(person);
+  } catch (error) {
+    res.status(500).json({ error: "Could not fetch person" });
+  }
+});
+
+app.put("/api/:user_id", async (req, res) => {
+  try {
+    const updatedPerson = await Person.findByIdAndUpdate(
+      req.params.user_id,
+      req.body,
+      { new: true }
+    );
+    if (!updatedPerson) {
+      return res.status(404).json({ error: "Person not found" });
     }
-  });
-  
-  app.delete('/api/:user_id', async (req, res) => {
-    try {
-      const person = await Person.findByIdAndDelete(req.params.user_id);
-      if (!person) {
-        return res.status(404).json({ error: 'Person not found' });
-      }
-      res.json({ message: 'Person deleted successfully' });
-    } catch (error) {
-      res.status(500).json({ error: 'Could not delete person' });
+    res.json(updatedPerson);
+  } catch (error) {
+    res.status(500).json({ error: "Could not update person" });
+  }
+});
+
+app.delete("/api/:user_id", async (req, res) => {
+  try {
+    const person = await Person.findByIdAndDelete(req.params.user_id);
+    if (!person) {
+      return res.status(404).json({ error: "Person not found" });
     }
-  });
+    res.json({ message: "Person deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ error: "Could not delete person" });
+  }
+});
 
 const PORT = process.env.PORT || 7777;
 app.listen(PORT, () => {
